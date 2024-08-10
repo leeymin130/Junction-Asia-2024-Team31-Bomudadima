6bc7851e18eb98488897af5166c5616134df587f
leeymin130 <138895117+leeymin130@users.noreply.github.com>
2024-08-10 23:49:34 +0900

    Add files via upload

---
+++ Bomudadima/BomudadimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BomudadimaApp.swift
+//  Bomudadima
+//
+//  Created by yoomin on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BomudadimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
